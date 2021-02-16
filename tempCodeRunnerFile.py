adc2e8bc0447840ca6d05a7ae7c1d9629badba28
Jsuyeon <65106676+Jsuyeon@users.noreply.github.com>
2021-02-16 20:41:00 +0900

    Delete tempCodeRunnerFile.py
    삭제

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-in range(1,n):
-    dp[i][0]=min(dp[i-1][0]+home[i][1],dp[i-1][0]+home[i][2])
-    dp[i][1]=min(dp[i-1][1]+home[i][0],dp[i-1][1]+home[i][2])
-    dp[i][2]=min(dp[i-1][2]+home[i][0],dp[i-1][2]+home[i][1])
-print(dp)
